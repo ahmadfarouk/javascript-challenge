34b4f6e99871ff386a80f4093f324735c104b9ae
ahmadfarouk <49321942+ahmadfarouk@users.noreply.github.com>
2021-01-15 22:07:48 -0500

    app.js code update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -18,3 +18,24 @@ tableData.forEach(function(xyz){
     })
 })
 
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    table_body.html("");
+    enter_date_input = d3.select("input")
+
+    filter_value = enter_date_input.property("value");
+    var filteredData = tableData.filter(data_datetime => data_datetime.datetime == filter_value);
+    //console.log (filteredData);
+
+    filteredData.forEach(function(xyz){
+        //console.log (xyz);
+        var added_row = table_body.append("tr");
+    
+        Object.entries(xyz).forEach(function([key, value]){
+            //console.log(key, value);
+            var new_td = added_row.append("td");
+            new_td.text(value);
+        })
+    })
+})
